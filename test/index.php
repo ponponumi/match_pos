29f1623497d07cd0227ab638865f8d7efb9ccb80
ponpon <ponponumi@gmail.com>
2024-03-23 13:24:32 +0900

    テスト用のファイルを作成

---
+++ test/index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$text = "878 re 882 885 985 fr 57";
+preg_match_all('/\d+/', $text, $matches);
+
+$numbers = $matches[0];
+
+var_dump($numbers);
