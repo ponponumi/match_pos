8c801a5413284dadf9cda575715fd92a17eef46c
ponpon <ponponumi@gmail.com>
2024-03-23 13:28:25 +0900

    テストで日本語文字を追加

--- test/index.php
+++ test/index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 use Ponponumi\MatchPos\Search;
 
-$text = "878 re 882 885 985 fr4 57";
+$text = "こんにちは 878 re 882 885 985 fr4 57";
 preg_match_all('/\d+/', $text, $matches);
 
 $numbers = $matches[0];
