cdced9c7444e598e7deb12a3aef7b7e53f6bfeb9
ponpon <ponponumi@gmail.com>
2024-03-23 13:27:23 +0900

    posを検索してみる機能を呼び出し

--- test/index.php
+++ test/index.php
@@ -2,9 +2,17 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
+use Ponponumi\MatchPos\Search;
+
 $text = "878 re 882 885 985 fr4 57";
 preg_match_all('/\d+/', $text, $matches);
 
 $numbers = $matches[0];
 
 var_dump($numbers);
+
+$single_list = Search::singlebyte($text,$numbers);
+var_dump($single_list);
+
+$multi_list = Search::multibyte($text,$numbers);
+var_dump($multi_list);
