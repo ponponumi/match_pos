813795cf222f7de3c8be88f3be6f1fe8eddbcfaf
ponpon <ponponumi@gmail.com>
2024-03-23 13:25:03 +0900

    テキストを編集

--- test/index.php
+++ test/index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
-$text = "878 re 882 885 985 fr 57";
+$text = "878 re 882 885 985 fr4 57";
 preg_match_all('/\d+/', $text, $matches);
 
 $numbers = $matches[0];
