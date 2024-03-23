300bfb23019354e4ffc19b9eaca66ec0c29468ec
ponpon <ponponumi@gmail.com>
2024-03-23 13:17:10 +0900

    コア部分のメソッドを作成

--- src/Search.php
+++ src/Search.php
@@ -3,5 +3,32 @@
 namespace Ponponumi\MatchPos;
 
 class Search{
-  //
+  public static function core(string $text,array $list,$multibyte_mode=true){
+    // 取得する
+    $result = [];
+
+    if($list !== []){
+      // リストがあれば
+      $start = 0;
+
+      foreach ($list as $item) {
+        if($multibyte_mode){
+          $pos = mb_strpos($text,$item,$start);
+        }else{
+          $pos = strpos($text,$item,$start);
+        }
+
+        if($pos !== false){
+          $start = $pos + 1;
+        }
+
+        $result[] = [
+          "pos" => $pos,
+          "value" => $item,
+        ];
+      }
+    }
+
+    return $result;
+  }
 }
